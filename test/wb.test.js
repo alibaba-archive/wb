99a52f4375b0323de96c3a937a1dec0c9ecabb92
fengmk2 <fengmk2@gmail.com>
2014-10-09 01:06:05 +0800

    first commit

---
+++ test/wb.test.js
@@ -0,0 +1,22 @@
+/**!
+ * wb - test/wb.test.js
+ *
+ * Copyright(c) 2014 fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var Weibo = require('../');
+
+describe('wb.test.js', function () {
+
+});
